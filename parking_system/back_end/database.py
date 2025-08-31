cbe19c23c9a2ba726a3c2c87455c128b1e2f3b69
hayato6624 <haya0908to@yahoo.co.jp>
2025-08-31 21:09:57 +1000

    Edit DATABASE_URL

--- parking_system/back_end/database.py
+++ parking_system/back_end/database.py
@@ -2,8 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://parking_memory_user:2qFKwMwjQbVo8ds42sYiTJmDpS265Lpy@dpg-d2q29bfdiees73cgpfcg-a:5432/parking_memory/"
+DATABASE_URL = "postgresql://parking_memory_user:2qFKwMwjQbVo8ds42sYiTJmDpS265Lpy@dpg-d2q29bfdiees73cgpfcg-a:5432/parking_memory"
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 Base = declarative_base()

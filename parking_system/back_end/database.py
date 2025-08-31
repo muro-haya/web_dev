d192fedb0742043cbff4c7e927d16f291cdd4bc6
hayato6624 <haya0908to@yahoo.co.jp>
2025-08-31 20:37:24 +1000

    environmental path chenge

--- parking_system/back_end/database.py
+++ parking_system/back_end/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite:///./parking.db"
+DATABASE_URL = "postgresql://parking_memory_user:2qFKwMwjQbVo8ds42sYiTJmDpS265Lpy@dpg-d2q29bfdiees73cgpfcg-a/parking_memory"
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)

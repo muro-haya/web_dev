f6c70b996bc86af54bb97935d1bec5c3b9ae9231
hayato6624 <haya0908to@yahoo.co.jp>
2025-08-31 21:38:02 +1000

    again

--- parking_system/back_end/main.py
+++ parking_system/back_end/main.py
@@ -14,7 +14,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://web-dev-gamma-three.vercel.app/"],
+    allow_origins=["https://web-dev-gamma-three.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],

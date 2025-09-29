f5293ff6aeb3ca4ddd583afdb97047716a3d4cbb
hayato6624 <haya0908to@yahoo.co.jp>
2025-09-29 12:03:08 +1000

    Changed DB

--- parking_system/back_end/database.py
+++ parking_system/back_end/database.py
@@ -2,7 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://parking_memory_user:2qFKwMwjQbVo8ds42sYiTJmDpS265Lpy@dpg-d2q29bfdiees73cgpfcg-a:5432/parking_memory"
+# DATABASE_URL = "postgresql://parking_memory_user:2qFKwMwjQbVo8ds42sYiTJmDpS265Lpy@dpg-d3cufkogjchc739hld4g-a:5432/parking_memory"
+DATABASE_URL = "postgresql://parking_memory_yk1p_user:3a23VP0ZSJo3vjODMJkV6bn98C4fBP2I@dpg-d3cufkogjchc739hld4g-a:5432/parking_memory_yk1p"
+# DATABASE_URL = "postgresql://parking_memory_yk1p_user:3a23VP0ZSJo3vjODMJkV6bn98C4fBP2I@dpg-d3cufkogjchc739hld4g-a.oregon-postgres.render.com/parking_memory_yk1p"
+
 # DATABASE_URL = "sqlite:///./parking_memory.db"
 
 engine = create_engine(DATABASE_URL, echo=True)
